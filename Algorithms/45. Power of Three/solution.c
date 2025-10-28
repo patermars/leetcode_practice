c836cbe1a3a96804acafa2eb5d63c10c5b466c23
PaterMars <patermars@outlook.com>
2025-10-28 23:00:59 +0530

    45. Power of Three

---
+++ Algorithms/45. Power of Three/solution.c
@@ -0,0 +1,19 @@
+bool isPowerOfThree(int n) {
+    if (n < 1) return false;
+    while(n!=1)
+    {
+        if(n%3==0)
+        {
+            n/=3;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if(n==1)
+    {
+        return true;
+    }
+    return false;
+}
